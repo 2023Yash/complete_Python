61ab6e86eb02f8255aa715f3054cdffbb06e6a60
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2024-07-04 22:15:59 +0530

    Update Project 2 Rock Paper Scissors.py

--- Project 2 Rock Paper Scissors.py
+++ Project 2 Rock Paper Scissors.py
@@ -1,4 +1,16 @@
 import random
-player = int(input("Write 1(Rock), 2(Paper) or 3(Scissors)"))
-computer = random.randrange(1,3)
-print(computer)
+
+def rock_paper_scissors(): 
+  player = int(input("Write 1(Rock), 2(Paper) or 3(Scissors):"))
+  computer = random.randrange(1,3)
+
+  if(player == computer):
+    print("Tie")
+  else:
+    if(player == 1 and computer == 2):
+      print("Win")
+    elif(player == 3 and computer == 2):
+
+  return rock_paper_scissors()
+
+rock_paper_scissors()
