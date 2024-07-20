5140713b4bba0078de4fa1c79ce91552debe6f0e
Yash Naudiyal <128122739+2023Yash@users.noreply.github.com>
2024-07-20 17:06:15 +0530

    Update Project 4 Random Number Guessing Game.py

--- Project 4 Random Number Guessing Game.py
+++ Project 4 Random Number Guessing Game.py
@@ -9,13 +9,13 @@ import random
 
 def Random_Number_Guessing_Game():
     random_number = random.randint(1, 100)
-    print("Guess a number between 1️⃣ and 1️⃣0️⃣0️⃣.")
+    print("Guess a number between 1️⃣  and 1️⃣ 0️⃣ 0️⃣  .")
     print()
     
-    for i in range(10):
+    for i in range(5):
         guess_input = int(input("Guess 🤔 a number: "))
         if guess_input == random_number:
-            print(f"You guessed the number right ✅! The number was {random_number}")
+            print(f"You guessed the number right ✅ ! The number was {random_number}")
             break
         else:
             if guess_input > random_number:
@@ -23,6 +23,6 @@ def Random_Number_Guessing_Game():
             elif guess_input < random_number:
                 print("The number is greater.")
     else:
-        print(f"You guessed the number wrong ❌! The number was {random_number}")
+        print(f"You guessed the number wrong ❌ ! The number was {random_number}")
 
 Random_Number_Guessing_Game()
